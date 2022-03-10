7db1fde9288e261000a064ff5ac688f2fc3a6c98
Franko Kirincic <frankokirincic@yahoo.com>
2022-03-10 22:24:39 +0100

    Create a working base.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Image, ModalGalleryConfig, ModalGalleryRef, ModalGalleryService } from '@ks89/angular-modal-gallery';
+import { ButtonsStrategy, Image, ModalGalleryConfig, ModalGalleryRef, ModalGalleryService, ModalLibConfig } from '@ks89/angular-modal-gallery';
 import { CarouselItem } from '../shared/carousel/carousel.component';
 import 'hammerjs';
 import 'mousetrap';
@@ -13,28 +13,21 @@ export class HomepageComponent implements OnInit {
   heroCarouselItems: CarouselItem[] = [];
   images: Image[] = []; // init this array with your images
 
+  constructor(private modalGalleryService: ModalGalleryService){
+    this.heroCarouselItems.push(new CarouselItem("assets/img/home_slider/balcony1.jpg", "Balcony", "Subtitle", "Read more"))
+    this.heroCarouselItems.push(new CarouselItem("assets/img/home_slider/garden1.jpg", "Garden", "Subtitle", "Read more"))
+
+    this.images = [...this.images, new Image(0, { img: "assets/img/home_slider/balcony1.jpg"})];
+    this.images = [...this.images, new Image(1, { img: "assets/img/home_slider/garden1.jpg"})];
+    // this.images.push(new Image(0, { img: "assets/img/home_slider/garden1.jpg"}))
+  }
+
   openModal(id: number, imageIndex: number): void {
     const dialogRef: ModalGalleryRef = this.modalGalleryService.open({
       id: id,
       images: this.images,
       currentImage: this.images[imageIndex],
-      libConfig: {
-        slideConfig: {
-          infinite: true,
-          sidePreviews: {
-            show: false
-          }
-        }
-      }
     } as ModalGalleryConfig) as ModalGalleryRef;
-  }
-
-  constructor(private modalGalleryService: ModalGalleryService){
-    this.heroCarouselItems.push(new CarouselItem("assets/img/home_slider/balcony1.jpg", "Balcony", "Subtitle", "Read more"))
-    this.heroCarouselItems.push(new CarouselItem("assets/img/home_slider/garden1.jpg", "Garden", "Subtitle", "Read more"))
-
-    this.images.push(new Image(0, { img: "assets/img/home_slider/balcony1.jpg"}))
-    this.images.push(new Image(0, { img: "assets/img/home_slider/garden1.jpg"}))
   }
 
   ngOnInit(): void {
